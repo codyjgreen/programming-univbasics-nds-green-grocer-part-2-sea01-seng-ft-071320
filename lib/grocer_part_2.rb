35efd08189debcc56cd249eda38db0b4e36d7b71
Cody Green <cody.green@flatironschool.com>
2020-09-18 16:29:14 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -53,7 +53,19 @@ end
 # Returns a float: a total of the cart
  
   
+# def checkout(cart, coupons)
+#   final_cart = apply_clearance(apply_coupons(consolidate_cart(cart), coupons))
+  
+#   total = 0
 
+#   final_cart.each do |item|
+#     total += item[:price] * item[:count]
+#   end
+
+#   total *= 0.9 if total > 100
+
+#   total.round(2)
+# end
 
 
 def checkout(cart, coupons)
